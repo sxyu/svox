a9d5beb27c4052eceba892c3ebe3295f1528d940
Alex Yu <sxyu@berkeley.edu>
2021-04-14 16:35:58 -0700

    Continue if failed to load cuda ext

--- setup.py
+++ setup.py
@@ -11,15 +11,20 @@ exec(open('svox/version.py', 'r').read())
 CUDA_FLAGS = []
 INSTALL_REQUIREMENTS = []
 
-ext_modules = [
-    CUDAExtension('svox.csrc', [
-        'svox/csrc/svox.cpp',
-        'svox/csrc/svox_kernel.cu',
-        'svox/csrc/rt_kernel.cu',
-        'svox/csrc/quantizer.cpp',
-    ], include_dirs=[osp.join(ROOT_DIR, "svox", "csrc", "include")],
-    optional=True),
-]
+try:
+    ext_modules = [
+        CUDAExtension('svox.csrc', [
+            'svox/csrc/svox.cpp',
+            'svox/csrc/svox_kernel.cu',
+            'svox/csrc/rt_kernel.cu',
+            'svox/csrc/quantizer.cpp',
+        ], include_dirs=[osp.join(ROOT_DIR, "svox", "csrc", "include")],
+        optional=True),
+    ]
+except:
+    import warnings
+    warnings.warn("Failed to build CUDA extension")
+    ext_modules = []
 
 setup(
     name='svox',
